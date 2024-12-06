25ec3bf027a5f814edc283da73f6dd9b9db144ae
Aaron Pinero <ap2963@cumc.columbia.edu>
2024-12-06 18:31:37 -0500

    changing port to 8080

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def process_string():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
