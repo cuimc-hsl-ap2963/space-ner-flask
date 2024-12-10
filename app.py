9fc66f1a391fe3f58c26b1cfd938611f1bc97659
Aaron Pinero <ap2963@cumc.columbia.edu>
2024-12-10 11:53:29 -0500

    added comments

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, request, jsonify
 
 import spacy
 
+# Create the Flask app.
 app = Flask(__name__)
 
-# Load the spaCy model globally
+# Load the spaCy model globally.
 nlp = spacy.load("en_core_web_trf")
 
+# Define the health check endpoint; this was added for future deployment to AWS App Runner.
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify(status="healthy"), 200
 
+# Define the NER service endpoint.
 @app.route('/process', methods=['POST'])
 def process_string():
     # Get the input string from the request
@@ -18,11 +21,14 @@ def process_string():
     
     # Peform NER on the string
     doc = nlp(input_data)
+
+    # Extract the ORG entities from the document.
     org_entities = [ent.text for ent in doc.ents if ent.label_ == "ORG"]
     result = org_entities
     
-    # Return the result as JSON
+    # Return the result as JSON.
     return jsonify(result)
 
+# Run the app.
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=False)
